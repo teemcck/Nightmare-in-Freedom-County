33dae8656ce4f869ee940aa2f04b648f94c566ed
tristanmac <mccaskilltristan@gmail.com>
2025-10-24 22:03:15 -0400

    Added MapHandler & Movement

---
+++ Assets/Scripts/GameScripts/MapHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Location {
+    UpperFloor,
+    MainFloor,
+    ExitHall,
+    Celler,
+    Kitchen,
+    Bathroom,
+    Bedroom,
+    MasterBedroom
+}
+
+public class MapHandler : MonoBehaviour
+{
+    private static MapHandler instance;
+
+    // Store origin point for each location.
+    private Dictionary<Location, Vector2> locationCordinates = new()
+    {
+        { Location.UpperFloor, new Vector2(0f, 0f) }, // 0,0 is placeholder for now.
+        { Location.MainFloor, new Vector2(0f, 0f) },
+        { Location.ExitHall, new Vector2(0f, 0f) },
+        { Location.Celler, new Vector2(0f, 0f) },
+        { Location.Kitchen, new Vector2(0f, 0f) },
+        { Location.Bathroom, new Vector2(0f, 0f) },
+        { Location.Bedroom, new Vector2(0f, 0f) },
+        { Location.MasterBedroom, new Vector2(0f, 0f) }
+    };
+
+    // Singleton pattern.
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        instance = this;
+    }
+    
+    public Vector2 GetLocation(Location location)
+    {
+        return locationCordinates[location];
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0.0f, 10.0f), SerializeField] private float defaultPlayerSpeed = 2f;
+
+    private float currentPlayerSpeed;
+    // For when the environmental slow effects xd.
+    [Range(0.0f, 1.0f), SerializeField] private float slownessSpeedDecrease = 0.4f;
+    private bool playerSlowed = false;
+
+    void Awake()
+    {
+        currentPlayerSpeed = defaultPlayerSpeed;
+    }
+
+    void Update()
+    {
+        ProcessKeyInput();
+    }
+
+    private void ProcessKeyInput()
+    {
+        Vector3 newPosition = transform.position;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           newPosition.y += currentPlayerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            newPosition.x -= currentPlayerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            newPosition.y -= currentPlayerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            newPosition.x += currentPlayerSpeed * Time.deltaTime;
+        }
+        transform.position = newPosition;
+    }
+    
+    public void SetSlowness(bool flag)
+    {
+        if (flag && !playerSlowed)
+        {
+            playerSlowed = true;
+            currentPlayerSpeed = defaultPlayerSpeed * (1 - slownessSpeedDecrease);
+        }
+        else
+        {
+            playerSlowed = false;
+            currentPlayerSpeed = defaultPlayerSpeed;
+        }
+    }
+}
