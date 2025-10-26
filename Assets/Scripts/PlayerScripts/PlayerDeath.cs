16f223f9badf4600d30b88b48f8aa0c074c311b3
tristanmac <mccaskilltristan@gmail.com>
2025-10-26 17:31:19 -0400

    Updated Florida

--- Assets/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerScripts/PlayerDeath.cs
@@ -1,19 +1,27 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 using UnityEngine;
-
-public enum DeathType
-{
-    Stabbing,
-    Sanity
-}
 
 public class PlayerDeath : MonoBehaviour
 {
-    [SerializeField] private int DeathSceneIndex = 0;
-    
-    public void TriggerPlayerDeath(DeathType type)
+    [SerializeField] private int ThreeHeartScene = 0;
+    [SerializeField] private int TwoHeartScene = 0;
+    [SerializeField] private int OneHeartScene = 0;
+    [SerializeField] private PlayerHealth playerHealth;
+
+    public void TriggerPlayerDeath()
     {
-        
+        playerHealth.CurrentPlayerHealth -= 1;
+        if (playerHealth.CurrentPlayerHealth == 2)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneAt(ThreeHeartScene));
+        }
+        else if (playerHealth.CurrentPlayerHealth == 1)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneAt(TwoHeartScene));
+        }
+        else if (playerHealth.CurrentPlayerHealth == 0)
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneAt(OneHeartScene));
+        }
     }
 }
